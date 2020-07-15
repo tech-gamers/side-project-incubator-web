2c801a77e9cab566ddd1d18b0882007d26f0fa63
Shawn Chen <ethanloatrchen@gmail.com>
2020-07-15 15:55:39 -0700

    Updates

---
+++ precache-manifest.f5426f298ad97d94523a72ea31908603.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "988ae4472667a6de938fd75cf840d44a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aef7f31f1fa8aff37b3",
+    "url": "/static/css/2.120bf8ef.chunk.css"
+  },
+  {
+    "revision": "c8e704940041e02e1e7c",
+    "url": "/static/css/main.bc1faa15.chunk.css"
+  },
+  {
+    "revision": "5aef7f31f1fa8aff37b3",
+    "url": "/static/js/2.5dac118e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5dac118e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e704940041e02e1e7c",
+    "url": "/static/js/main.94f5e8c0.chunk.js"
+  },
+  {
+    "revision": "343f9338856b7bfa8781",
+    "url": "/static/js/runtime-main.42882f35.js"
+  }
+]);
