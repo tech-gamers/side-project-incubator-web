2939ee3db4abaf1691f0bf524c2f466172439c1e
Shawn Chen <ethanloatrchen@gmail.com>
2020-07-15 19:14:32 -0700

    Add route, hide wechat button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 import { Layout } from 'antd';
 import Home from './home/Home';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 const { Content, Footer } = Layout;
 
-function App() {
-  return (
-    <Layout>
-      <header className="spi-app__header">
-        <section className="container">
-          <div className="spi-app__logo">
-            <img src="/assets/images/logo.png" alt="logo" />
-            玩儿技术
-          </div>
-        </section>
-      </header>
-      <Content className="spi-app__content">
-        <Home />
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>©2020 Created by Tech Gamers</Footer>
-    </Layout>
-  );
+class App extends Component<{}, {}> {
+  render(){
+    return (
+      <Layout>
+        <header className="spi-app__header">
+          <section className="container">
+            <div className="spi-app__logo">
+              <img src="/assets/images/logo.png" alt="logo" />
+              玩儿技术
+            </div>
+          </section>
+        </header>
+        <Content className="spi-app__content">
+          <Router>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Router>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>©2020 Created by Tech Gamers</Footer>
+      </Layout>
+    );
+  }
 }
 
 export default App;
--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -5,20 +5,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faWeixin } from '@fortawesome/free-brands-svg-icons';
 
 class Home extends Component<{}, {}> {
+  constructor(props: any) {
+    super(props);
+    this.onGithubLogin = this.onGithubLogin.bind(this);
+  }
+
+  /**
+   * Login use github
+   */
+  onGithubLogin(): void {
+    window.location.href = '/users/auth/github';
+  }
+
   render() {
     return (
       <div className="container mt-5">
         <h2>欢迎来到【玩儿技术】社区</h2>
-        <h2>在这里找到志同道合的技术热爱者一起享受创造的乐趣！ 社区网站正在从零开始搭建当中。我们需要你的协助</h2>
+        <h2>在这里找到志同道合的技术热爱者一起享受创造的乐趣！社区网站正在从零开始搭建当中。我们需要你的协助</h2>
         <br />
-        <Button type="default" size="large" block>
+        <Button type="default" size="large" block onClick={this.onGithubLogin}>
           Sign in with Github &nbsp;
           <FontAwesomeIcon icon={faGithub} />
         </Button>
-        <Button type="default" size="large" block className="mt-3 spi-home__signin-wechat">
+        {/* <Button type="default" size="large" block className="mt-3 spi-home__signin-wechat">
           Sign in with Wechat &nbsp;
           <FontAwesomeIcon icon={faWeixin} />
-        </Button>
+        </Button> */}
       </div>
     );
   }
