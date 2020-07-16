7404c57d52af9683aeb3c6cde65e0ea724449dd7
Tianjian Wang <wangtjwork@gmail.com>
2020-07-15 19:03:01 -0700

    Text Change: Delete additional space after punctuation mark

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -9,7 +9,7 @@ class Home extends Component<{}, {}> {
     return (
       <div className="container mt-5">
         <h2>欢迎来到【玩儿技术】社区</h2>
-        <h2>在这里找到志同道合的技术热爱者一起享受创造的乐趣！ 社区网站正在从零开始搭建当中。我们需要你的协助</h2>
+        <h2>在这里找到志同道合的技术热爱者一起享受创造的乐趣！社区网站正在从零开始搭建当中。我们需要你的协助</h2>
         <br />
         <Button type="default" size="large" block>
           Sign in with Github &nbsp;
