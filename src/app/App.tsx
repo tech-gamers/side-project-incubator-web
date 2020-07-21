86f16096bd8e83d90b0300474bec8bcac40577d9
Tianjian Wang <wangtjwork@gmail.com>
2020-07-21 01:04:41 -0700

    [UI] 404 page & error page (#7)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,30 +6,35 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from './dashboard/Dashboard';
 import Header from './header/Header'
 import User from './user/User';
+import ErrorBoundary from './ErrorBoundary';
+import NoMatch from './NoMatch';
 
 const { Content, Footer } = Layout;
 
 class App extends Component<{}, {}> {
   render(){
     return (
-      <Router>
-        <Layout>
-          {/* Header */}
-          <Header />
+      <ErrorBoundary>
+        <Router>
+          <Layout>
+            {/* Header */}
+            <Header />
 
-          {/* Content */}
-          <Content className="spi-app__content">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/dashboard" component={Dashboard} />
-              <Route exact path="/users/:userId" component={User} />
-            </Switch>
-          </Content>
+            {/* Content */}
+            <Content className="spi-app__content">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/dashboard" component={Dashboard} />
+                <Route exact path="/users/:userId" component={User} />
+                <Route component={NoMatch}/>
+              </Switch>
+            </Content>
 
-          {/* Footer */}
-          <Footer className="text-center">©2020 Created by Tech Gamers</Footer>
-        </Layout>
-      </Router>
+            {/* Footer */}
+            <Footer className="text-center">©2020 Created by Tech Gamers</Footer>
+          </Layout>
+        </Router>
+      </ErrorBoundary>
     );
   }
 }
