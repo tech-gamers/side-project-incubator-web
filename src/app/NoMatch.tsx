a8d5fd9484befe74234d7ab5abb8d71927b7c2f5
Yang Liu <JeremyRobturtle@gmail.com>
2020-07-24 20:15:03 -0700

    [TOOLING] setup ESLint w/ react hooks linters (#18)

--- src/app/NoMatch.tsx
+++ src/app/NoMatch.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export default () => (
-  <div style={{textAlign: 'center'}}>
-    <h1>404 PAGE NOT FOUND</h1>
-    <h2>互联网是有记忆的</h2>
-    <h3>网络不是法外之地</h3>
-    <h6>说你呢，别在浏览器里面输随机url了，我们真有丑爆了的404页面</h6>
-  </div>
-)
+export default function NoMatch() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>404 PAGE NOT FOUND</h1>
+      <h2>互联网是有记忆的</h2>
+      <h3>网络不是法外之地</h3>
+      <h6>说你呢，别在浏览器里面输随机url了，我们真有丑爆了的404页面</h6>
+    </div>
+  );
+}
