75372754ecaaeec6508434a9ffd13afbe5e13ae2
Yang Liu <JeremyRobturtle@gmail.com>
2020-07-27 00:18:26 -0700

    [API] send w/ CSRF token (#22)

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import './Home.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import useCsrfToken from 'app/hooks/useCsrfToken';
 
 export default function Home() {
+  const [csrfToken] = useCsrfToken();
   return (
     <div className="container mt-5">
       <h2>欢迎来到【玩儿技术】社区</h2>
       <h2>在这里找到志同道合的技术热爱者一起享受创造的乐趣！社区网站正在从零开始搭建当中。我们需要你的协助</h2>
       <br />
       <form action="https://api.tech-gamers.live/auth/github" method="post">
-        <button type="submit" className="ant-btn ant-btn-lg ant-btn-block">
+        <input type="hidden" name="authenticity_token" value={csrfToken} />
+        <button type="submit" className="ant-btn ant-btn-lg ant-btn-block" disabled={csrfToken.length === 0}>
           Sign in with Github &nbsp;
           <FontAwesomeIcon icon={faGithub} />
         </button>
       </form>
-    </div >
+    </div>
   );
 }
--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -2,17 +2,58 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.tech-gamers.live/';
 
-export const api = {
-  get(url: string) {
-    return axios.get(`${BASE_URL}${url}`);
+type RESTMethod = 'get' | 'post' | 'put' | 'patch' | 'delete';
+
+type API = {
+  [key in RESTMethod]: (url: string) => Promise<any>;
+} & {
+  request: (method: RESTMethod, url: string) => Promise<any>;
+};
+
+type CSRFTracker = {
+  csrfToken: string;
+  handshake: () => Promise<string>;
+};
+
+export const api: API & CSRFTracker = {
+  async get(url: string) {
+    return await this.request('get', url);
   },
-  post(url: string) {
-    return axios.post(`${BASE_URL}${url}`);
+  async post(url: string) {
+    return await this.request('post', url);
   },
-  put(url: string) {
-    return axios.put(`${BASE_URL}${url}`);
+  async put(url: string) {
+    return await this.request('put', url);
   },
-  delete(url: string) {
-    return axios.delete(`${BASE_URL}${url}`);
+  async patch(url: string) {
+    return await this.request('patch', url);
+  },
+  async delete(url: string) {
+    return await this.request('delete', url);
+  },
+
+  csrfToken: '',
+  async handshake() {
+    const res = await this.request('post', 'auth/handshake');
+    this.csrfToken = res.headers['x-csrf-token'];
+    return this.csrfToken;
+  },
+
+  // TODO: a global error handler
+  // TODO: auto handshake and retry on 422
+  async request(method: RESTMethod, url: string) {
+    const res = await axios.request({
+      method: method,
+      baseURL: BASE_URL,
+      url: url,
+      xsrfCookieName: '_SPI_session',
+      xsrfHeaderName: 'X-CSRF-Token',
+      headers: {
+        'X-CSRF-Token': this.csrfToken
+      },
+      withCredentials: true
+    });
+    this.csrfToken = res.headers['x-csrf-token'];
+    return res;
   }
 };
